3246332ffe622eee0b60ac302101ccb2d55e4180
pwang33 <pwang33@illinois.edu>
2021-06-14 17:30:08 -0700

    renamed index.py to application.py for deployment on Elastic BeanStack

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
-app.title = 'A Bite of China'
+app = dash.Dash(__name__, suppress_callback_exceptions=True, title='A Bite of China', assets_folder='static',
+                assets_url_path='static')
 server = app.server
